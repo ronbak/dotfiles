7300734ae7ebe7bc2e437a4514cfd69efefae807
Jonas Peter <jonas.peter@gmx.com>
2015-07-23 20:52:38 +0200

    added hotkeys for langtool

--- init.el
+++ init.el
@@ -27,3 +27,18 @@
 ;;append the directory name to not unique file (buffer) names
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward)
+
+
+;;;;dedicated window for popup notifacations
+(defun toggle-current-window-dedication ()
+ (interactive)
+ (let* ((window    (selected-window))
+        (dedicated (window-dedicated-p window)))
+   (set-window-dedicated-p window (not dedicated))
+   (message "Window %sdedicated to %s"
+            (if dedicated "no longer " "")
+            (buffer-name))))
+
+(global-set-key [pause] 'toggle-current-window-dedication)
+
+(tool-bar-mode -1)
