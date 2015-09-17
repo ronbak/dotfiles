94c8455c5b03f0799ef87e26f129bdf8ec029054
Jonas Peter <jonas.peter@gmx.com>
2015-09-17 20:59:46 +0200

    added init for inline pictures in org-mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,4 +3,8 @@
   (add-hook 'org-mode-hook  (lambda () (setq truncate-lines nil)))
   (add-hook 'org-mode-hook  (lambda () (flyspell-mode t))))
 
+;;for displaying embedded pictures in the buffer
+(setq org-image-actual-width nil)
+(setq org-startup-with-inline-images t)
+
 (provide 'init-org)
