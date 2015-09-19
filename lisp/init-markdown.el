74802297369e36aa5d6ad267549756c19076db1d
Jonas Peter <jonas.peter@gmx.com>
2015-09-19 16:03:57 +0200

    added flyspell to markdown

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,4 +3,7 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
+(with-eval-after-load 'markdown-mode
+  (add-hook 'markdown-mode-hook  (lambda () (flyspell-mode t))))
+
 (provide 'init-markdown)
