21eabd6122e8c2c3fdb56f5d1d3496b506e5ca6d
Jonas Peter <jonas.peter@gmx.com>
2015-10-22 21:40:42 +0200

    use helm find file

--- .emacs/lisp/init-helm.el
+++ .emacs/lisp/init-helm.el
@@ -2,6 +2,9 @@
 (require 'helm-config)
 (helm-mode 1)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+
 (require-package 'helm-projectile)
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
