97f79e02b1d9858dcfddd19c58dfbbf5151a1454
Jonas Peter <jonas.peter@gmx.com>
2015-12-01 21:28:51 +0100

    added tests autoreload to lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.1"]
                   [refactor-nrepl "1.1.0"]
-                  [com.jakemccrary/lein-test-refresh "0.8.0"]]}}
+                  [com.jakemccrary/lein-test-refresh "0.8.0"]]
+       :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
